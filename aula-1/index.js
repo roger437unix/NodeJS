4887e7f581110511f1538f91a73319b806d32b72
roger437unix <33252885+roger437unix@users.noreply.github.com>
2025-02-20 07:54:47 -0300

    Update index.js

--- aula-1/index.js
+++ aula-1/index.js
@@ -7,15 +7,21 @@ console.log("Hello, World!")
 }
 console.log(nome)
 
-// Variável mutável e imutável 
+/**************************
+Variável mutável e imutável
+
+const => imutável | local
+let   => mutável  | local       
+var   => mutável  | global
+***************************/
 
 // const nome2 = "multics"
 let nome2 = "multics"
-
 console.log(nome2)
 
 nome2 = "unix"
 console.log(nome2)
 
+// Nomes semânticos
 const processCurrentWorkDirectory = process.cwd()
 console.log(processCurrentWorkDirectory)
